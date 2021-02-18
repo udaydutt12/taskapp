07282a32ea053a572a4cf0c740db925cc4aa7bef
udaydutt12 <udaydutt@utexas.edu>
2021-02-18 14:45:32 -0600

    Update __init__.py

--- taskapp/__init__.py
+++ taskapp/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 if getenv("PRODUCTION"):
